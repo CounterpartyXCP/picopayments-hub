a8af3cd8da6abf93c0d83ec55bb4b07ba37217cd
F483 <fabian.barkhau@gmail.com>
2016-11-10 21:49:17 +0100

    fixed api_mph_terms_test

--- tests/api_mph_terms_test.py
+++ tests/api_mph_terms_test.py
@@ -1,75 +1,59 @@
-import os
-import shutil
-import unittest
 import tempfile
+import pytest
+
+# this is require near the top to do setup of the test suite
+# from counterpartylib.test import conftest
+
 from picopayments import api
-from picopayments import srv
+from counterpartylib.test.util_test import CURR_DIR as CPLIB_TESTDIR
 
 
-CP_URL = os.environ.get("COUNTERPARTY_URL", "http://127.0.0.1:14000/api/")
+FIXTURE_SQL_FILE = CPLIB_TESTDIR + '/fixtures/scenarios/unittest_fixture.sql'
+FIXTURE_DB = tempfile.gettempdir() + '/fixtures.unittest_fixture.db'
 
 
-class TestMpcHubTerms(unittest.TestCase):
+@pytest.mark.usefixtures("picopayments_server")
+def test_standard_usage_xcp():
+    # TODO test input validation
 
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp(prefix="picopayments_test_")
-        self.basedir = os.path.join(self.tempdir, "basedir")
-        shutil.copytree("tests/fixtures", self.basedir)
-        srv.main([
-            "--testnet",
-            "--basedir={0}".format(self.basedir),
-            "--cp_url={0}".format(CP_URL)
-        ], serve=False)
+    # test gets all
+    terms = api.mph_terms()
+    assert(terms == {
+        "BTC": {
+            "deposit_max": 0,
+            "deposit_min": 0,
+            "deposit_ratio": 1.0,
+            "expire_max": 0,
+            "expire_min": 0,
+            "sync_fee": 1
+        },
+        "XCP": {
+            "deposit_max": 0,
+            "deposit_min": 0,
+            "deposit_ratio": 1.0,
+            "expire_max": 0,
+            "expire_min": 0,
+            "sync_fee": 1
+        },
+        "A14456548018133352000": {
+            "deposit_max": 0,
+            "deposit_min": 0,
+            "deposit_ratio": 1.0,
+            "expire_max": 0,
+            "expire_min": 0,
+            "sync_fee": 1
+        }
+    })
 
-    def tearDown(self):
-        shutil.rmtree(self.tempdir)
-
-    @unittest.skip("FIXME setup mock counterpartylib")
-    def test_standard_usage_xcp(self):
-        # TODO test input validation
-
-        # test gets all
-        terms = api.mph_terms()
-        self.assertEqual(terms, {
-            "BTC": {
-                "deposit_max": 0,
-                "deposit_min": 0,
-                "deposit_ratio": 1.0,
-                "expire_max": 0,
-                "expire_min": 0,
-                "sync_fee": 1
-            },
-            "XCP": {
-                "deposit_max": 0,
-                "deposit_min": 0,
-                "deposit_ratio": 1.0,
-                "expire_max": 0,
-                "expire_min": 0,
-                "sync_fee": 1
-            },
-            "A14456548018133352000": {
-                "deposit_max": 0,
-                "deposit_min": 0,
-                "deposit_ratio": 1.0,
-                "expire_max": 0,
-                "expire_min": 0,
-                "sync_fee": 1
-            }
-        })
-
-        # test limits to asset
-        terms = api.mph_terms(assets=["XCP"])
-        self.assertEqual(terms, {
-            "XCP": {
-                "deposit_max": 0,
-                "deposit_min": 0,
-                "deposit_ratio": 1.0,
-                "expire_max": 0,
-                "expire_min": 0,
-                "sync_fee": 1
-            },
-        })
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # test limits to asset
+    terms = api.mph_terms(assets=["XCP"])
+    assert(terms == {
+        "XCP": {
+            "deposit_max": 0,
+            "deposit_min": 0,
+            "deposit_ratio": 1.0,
+            "expire_max": 0,
+            "expire_min": 0,
+            "sync_fee": 1
+        },
+    })
