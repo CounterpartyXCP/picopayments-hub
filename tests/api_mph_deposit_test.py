a85b86fb19633f364b24ec93198b4420043b27a7
F483 <fabian.barkhau@gmail.com>
2016-11-14 21:54:50 +0100

    fixed mpc_deposit test

--- tests/api_mph_deposit_test.py
+++ tests/api_mph_deposit_test.py
@@ -1,22 +1,28 @@
-import os
-import shutil
-import unittest
 import tempfile
-import jsonschema
+import pytest
+
+# this is require near the top to do setup of the test suite
+# from counterpartylib.test import conftest
+
+from counterpartylib.test.util_test import CURR_DIR as CPLIB_TESTDIR
+from counterpartylib.test.fixtures.params import DP
+from micropayment_core.keys import address_from_wif
+from picopayments import lib
 from picopayments import api
 from picopayments_client import auth
-from picopayments import lib
-from picopayments import srv
+import os
 from picopayments import err
 from micropayment_core import util
 from micropayment_core import keys
 from micropayment_core.scripts import compile_deposit_script
 
 
-CP_URL = os.environ.get("COUNTERPARTY_URL", "http://127.0.0.1:14000/api/")
-
-
-DEPOSIT_RESULT_SCHEMA = {
+FIXTURE_SQL_FILE = CPLIB_TESTDIR + '/fixtures/scenarios/unittest_fixture.sql'
+FIXTURE_DB = tempfile.gettempdir() + '/fixtures.unittest_fixture.db'
+ASSET = "XCP"
+FUNDING_WIF = DP["addresses"][0][2]  # XTC: 91950000000, BTC: 199909140
+FUNDING_ADDRESS = address_from_wif(FUNDING_WIF)
+DEPOSIT_RESULT_SCHEMA = {  # FIXME check deposit result schema
     "type": "object",
     "properties": {
         "deposit_script": {"type": "string"},
@@ -31,36 +37,49 @@ DEPOSIT_RESULT_SCHEMA = {
 }
 
 
-class TestMpcHubDeposit(unittest.TestCase):
+@pytest.mark.usefixtures("picopayments_server")
+def test_validate_handle_exists():
 
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp(prefix="picopayments_test_")
-        self.basedir = os.path.join(self.tempdir, "basedir")
-        shutil.copytree("tests/fixtures", self.basedir)
-        srv.main([
-            "--testnet",
-            "--basedir={0}".format(self.basedir),
-            "--cp_url={0}".format(CP_URL)
-        ], serve=False)
+    try:
 
-    def tearDown(self):
-        shutil.rmtree(self.tempdir)
+        asset = "XCP"
+        client_key = lib.create_key(asset, netcode="XTN")
+        next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
+        c2h_deposit_script = util.b2h(os.urandom(32)),
 
-    @unittest.skip("FIXME setup mock counterpartylib")
-    def test_standard_usage_xcp(self):
+        params = {
+            "handle": "deadbeef",
+            "deposit_script": c2h_deposit_script,
+            "next_revoke_secret_hash": next_revoke_secret_hash
+        }
+        privkey = keys.wif_to_privkey(client_key["wif"])
+        params = auth.sign_json(params, privkey)
+        api.mph_deposit(**params)
+        assert False
+    except err.HandleNotFound:
+        assert True
+
+
+@pytest.mark.usefixtures("picopayments_server")
+def test_validate_deposit_not_already_given():
+
+    try:
 
         asset = "XCP"
         client_key = lib.create_key(asset, netcode="XTN")
         client_pubkey = client_key["pubkey"]
 
         h2c_spend_secret = util.b2h(os.urandom(32))
-        h2c_spend_secret_hash = util.hash160hex(h2c_spend_secret)
+        h2c_spend_secret_hash = util.hash160hex(
+            h2c_spend_secret
+        )
 
         params = {
             "asset": asset,
             "spend_secret_hash": h2c_spend_secret_hash
         }
-        params = auth.sign_json(params, keys.wif_to_privkey(client_key["wif"]))
+        privkey = keys.wif_to_privkey(client_key["wif"])
+        params = auth.sign_json(params, privkey)
         result = api.mph_request(**params)
 
         handle = result["handle"]
@@ -71,95 +90,30 @@ class TestMpcHubDeposit(unittest.TestCase):
             client_pubkey, hub_pubkey, c2h_spend_secret_hash, 1337
         )
 
+        # submit deposit
+        next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
+
+        params = {
+            "handle": handle,
+            "deposit_script": c2h_deposit_script,
+            "next_revoke_secret_hash": next_revoke_secret_hash
+        }
+        privkey = keys.wif_to_privkey(client_key["wif"])
+        params = auth.sign_json(params, privkey)
+        result = api.mph_deposit(**params)
+        assert result is not None
+
+        # resubmit deposit
         next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
         params = {
             "handle": handle,
             "deposit_script": c2h_deposit_script,
             "next_revoke_secret_hash": next_revoke_secret_hash
         }
-        params = auth.sign_json(params, keys.wif_to_privkey(client_key["wif"]))
+        privkey = keys.wif_to_privkey(client_key["wif"])
+        params = auth.sign_json(params, privkey)
         result = api.mph_deposit(**params)
 
-        self.assertIsNotNone(result)
-        jsonschema.validate(result, DEPOSIT_RESULT_SCHEMA)
-
-    @unittest.skip("FIXME setup mock counterpartylib")
-    def test_validate_deposit_not_already_given(self):
-
-        def func():
-
-            asset = "XCP"
-            client_key = lib.create_key(asset, netcode="XTN")
-            client_pubkey = client_key["pubkey"]
-
-            h2c_spend_secret = util.b2h(os.urandom(32))
-            h2c_spend_secret_hash = util.hash160hex(
-                h2c_spend_secret
-            )
-
-            params = {
-                "asset": asset,
-                "spend_secret_hash": h2c_spend_secret_hash
-            }
-            privkey = keys.wif_to_privkey(client_key["wif"])
-            params = auth.sign_json(params, privkey)
-            result = api.mph_request(**params)
-
-            handle = result["handle"]
-            hub_pubkey = result["pubkey"]
-            c2h_spend_secret_hash = result["spend_secret_hash"]
-
-            c2h_deposit_script = compile_deposit_script(
-                client_pubkey, hub_pubkey, c2h_spend_secret_hash, 1337
-            )
-
-            # submit deposit
-            next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
-
-            params = {
-                "handle": handle,
-                "deposit_script": c2h_deposit_script,
-                "next_revoke_secret_hash": next_revoke_secret_hash
-            }
-            privkey = keys.wif_to_privkey(client_key["wif"])
-            params = auth.sign_json(params, privkey)
-            result = api.mph_deposit(**params)
-            self.assertIsNotNone(result)
-
-            # resubmit deposit
-            next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
-            params = {
-                "handle": handle,
-                "deposit_script": c2h_deposit_script,
-                "next_revoke_secret_hash": next_revoke_secret_hash
-            }
-            privkey = keys.wif_to_privkey(client_key["wif"])
-            params = auth.sign_json(params, privkey)
-            result = api.mph_deposit(**params)
-
-        self.assertRaises(err.DepositAlreadyGiven, func)
-
-    @unittest.skip("FIXME setup mock counterpartylib")
-    def test_validate_handle_exists(self):
-
-        def func():
-
-            asset = "XCP"
-            client_key = lib.create_key(asset, netcode="XTN")
-            next_revoke_secret_hash = util.hash160hex(util.b2h(os.urandom(32)))
-            c2h_deposit_script = util.b2h(os.urandom(32)),
-
-            params = {
-                "handle": "deadbeef",
-                "deposit_script": c2h_deposit_script,
-                "next_revoke_secret_hash": next_revoke_secret_hash
-            }
-            privkey = keys.wif_to_privkey(client_key["wif"])
-            params = auth.sign_json(params, privkey)
-            api.mph_deposit(**params)
-
-        self.assertRaises(err.HandleNotFound, func)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert False
+    except err.DepositAlreadyGiven:
+        assert True
