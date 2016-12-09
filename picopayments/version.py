13c9f7250180b491b8f34ebab57adcbe02fc34a2
F483 <fabian.barkhau@gmail.com>
2016-12-09 22:18:18 +0100

    v0.0.9

--- picopayments/version.py
+++ picopayments/version.py
@@ -6,4 +6,4 @@
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
-__version__ = '0.0.8'
+__version__ = '0.0.9'
